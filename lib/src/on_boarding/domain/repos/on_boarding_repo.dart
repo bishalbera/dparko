cfd7a02bafb09f324f808a968d58b9171d77d384
bishalbera <bishalbera2@gmail.com>
2024-06-26 09:36:16 +0530

    on boarding screen added

--- lib/src/on_boarding/domain/repos/on_boarding_repo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:parko/core/utils/typedef.dart';
-
-abstract class OnBoardingRepo {
-  const OnBoardingRepo();
-
-  ResultFuture<void> cacheFirstTimer();
-
-  ResultFuture<bool> checkIfUserIsFirstTimer();
-}
